373e52224334f6813615aa3927b2c5dfe51faa7b
Simon Wintereder <41189046+wintersim@users.noreply.github.com>
2020-02-22 19:24:17 +0100

    Update createDb.sh

--- createDb.sh
+++ createDb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sqlite3 cat.db "CREATE TABLE Cat(Id INT, Name TEXT);"
-sqlite3 cat.db "CREATE TABLE Clicks(Name TEXT, Clicks INT);"
+sqlite3 cat.db "CREATE TABLE IF NOT EXISTS Cat(Id INTEGER PRIMARY KEY, Name TEXT);"
+sqlite3 cat.db "CREATE TABLE IF NOT EXISTS Clicks(Name TEXT PRIMARY KEY, Clicks INT);"
 sqlite3 cat.db "INSERT INTO Clicks VALUES('cat',0)"
